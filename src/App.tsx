fe59e66a54f3cf4dbc70589ff6c25b2b98ce28de
Vladislav110 <matsukvlad399@gmail.com>
2023-10-10 17:13:35 +0300

    Revert "Initial commit"
    This reverts commit 11ab70e4ff300546a25bd9f92227139d593c2e08.

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography } from "@/components/ui/typography";
-
-export function App() {
-  return <Typography.H1>Hello</Typography.H1>;
-}
--- src/components/previews/value-preview/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-export const ValuePreview = ({ children }: { children: ReactNode }) => {
-  return (
-    <div style={{ color: "white", mixBlendMode: "difference" }}>{children}</div>
-  );
-};
