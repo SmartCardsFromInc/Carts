abca9714ecdaa05bff9d1ffc778605e2063d55b8
Vladislav110 <matsukvlad399@gmail.com>
2023-10-10 17:26:51 +0300

    init commit

---
+++ src/components/previews/value-preview/index.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export const ValuePreview = ({ children }: { children: ReactNode }) => {
+  return (
+    <div style={{ color: "white", mixBlendMode: "difference" }}>{children}</div>
+  );
+};
