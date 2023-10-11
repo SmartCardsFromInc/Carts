91347a72a442b634eb7187a315f041036ae5a9cd
Maxim Alenchikov <maxim.alenchikov@gmail.com>
2023-10-11 23:29:17 +0300

    testM

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@/components/ui/typography";
 
 export function App() {
-  return <Typography.H1>Are you ready</Typography.H1>;
+  return <Typography.H1>Are you ready? Always!</Typography.H1>;
 }
