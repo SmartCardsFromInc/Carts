0e20e085f229c9337829d514e6499a402913ff7a
Vladislav110 <matsukvlad399@gmail.com>
2023-10-11 11:30:09 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@/components/ui/typography";
 
 export function App() {
-  return <Typography.H1>Hello</Typography.H1>;
+  return <Typography.H1>Are you ready</Typography.H1>;
 }
