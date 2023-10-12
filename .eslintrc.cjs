c5e9a41da6872748bf0aedc201799cd67dbab17c
LapkoAleksandr1992 <lapko-aleksandr@mail.ru>
2023-10-12 13:20:08 +0300

    "Updated .eslintrc.cjs "

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,11 @@
 module.exports = {
-  extends: ['@it-incubator/eslint-config', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
-  rules: { 'no-console': ['warn', { allow: ['warn', 'error'] }] },
-}
+  extends: [
+    "@it-incubator/eslint-config",
+    "plugin:storybook/recommended",
+    "plugin:storybook/recommended",
+  ],
+  rules: {
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+};
